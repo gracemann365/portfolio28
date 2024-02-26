82d1dbfd80809ae536a57e5a3ce52eb971bbd87a
gracemann365 <gracemann365@gmail.com>
2024-02-26 08:55:10 +0530

    functionTest

--- send_mail.php
+++ send_mail.php
@@ -1,21 +1,16 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-
-    // Email settings
-    $to = "gracemann365@gmail.com"; // Enter your email address here
-    $subject = "New message from website contact form";
-    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
-
-    // Send email
-    if (mail($to, $subject, $body)) {
-        // Redirect back to the main page
-        echo "<script>window.location.href = 'index.html';</script>";
-        exit();
-    } else {
-        echo "Failed to send message. Please try again later.";
-    }
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "youremail@mail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
 }
+//redirect
+header("Location:thankyou.html");
 ?>
